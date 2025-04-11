00ad76a52d626b6d29c7a5ed05b7333b1bb63f46
Björn Saemundsson <bjornorri@gmail.com>
2025-04-11 09:29:45 +0200

    Move hyper from enter to semicolon and add Ctrl to slash

--- keyboards/keychron/q1_max/ansi_encoder/keymaps/bjornorri/keymap.c
+++ keyboards/keychron/q1_max/ansi_encoder/keymaps/bjornorri/keymap.c
@@ -19,7 +19,9 @@
 
 #define MY_ESC C(G(KC_Q))
 #define MY_CAPS LT(1, KC_ESC)
-#define MY_ENT MT(MOD_HYPR, KC_ENT)
+#define MY_ENT MT(MOD_LSFT, KC_ENT)
+#define MY_SCLN MT(MOD_HYPR, KC_SCLN)
+#define MY_SLSH MT(MOD_RCTL, KC_SLSH)
 #define MY_TAB MT(MOD_LCTL, KC_TAB)
 #define MY_TRGR G(KC_BSLS)
 #define MY_LBRC LSG(KC_LBRC)
@@ -41,8 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         MY_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_MPLY,            KC_MUTE,
         KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
         MY_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-        MY_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,            MY_ENT,             MY_TRGR,
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,            KC_RSFT,  KC_UP,
+        MY_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     MY_SCLN,  KC_QUOT,            MY_ENT,             MY_TRGR,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   MY_SLSH,            KC_RSFT,  KC_UP,
         KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD,KC_ROPTN,    MO(3),  KC_LEFT,  KC_DOWN,  KC_RGHT),
 
     [1] = LAYOUT_ansi_82(
@@ -88,6 +90,10 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
         case MY_ENT:
             return true;
         case MY_TAB:
+            return true;
+        case MY_SCLN:
+            return true;
+        case MY_SLSH:
             return true;
     }
     return false;
