c1ab42461fc92d4269ba5f5db4ae77d76cdd8108
Björn Saemundsson <bjornorri@gmail.com>
2025-06-17 20:40:10 +0200

    Change keys below knob to MPLY, PGUP, PGDN.

--- users/bjornorri/bjornorri.c
+++ users/bjornorri/bjornorri.c
@@ -27,7 +27,6 @@ enum custom_keycodes { MY_ESC = MY_SAFE_RANGE, MY_TAB, MY_SCLN };
 #define L0_CAPS LT(1, KC_ESC)
 #define L0_ENT MT(MOD_LSFT, KC_ENT)
 #define L0_SLSH MT(MOD_RCTL, KC_SLSH)
-#define L0_TRGR G(KC_BSLS)
 
 // Custom keycodes.
 #define L0_ESC MY_ESC
@@ -55,9 +54,9 @@ enum custom_keycodes { MY_ESC = MY_SAFE_RANGE, MY_TAB, MY_SCLN };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_ansi_82(
         L0_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,   KC_MPLY,            KC_MUTE,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_PGUP,
-        L0_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGDN,
-        L0_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     L0_SCLN,  KC_QUOT,            L0_ENT,             L0_TRGR,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,   KC_BSPC,            KC_MPLY,
+        L0_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,            KC_PGUP,
+        L0_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     L0_SCLN,  KC_QUOT,            L0_ENT,             KC_PGDN,
         KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   L0_SLSH,            KC_RSFT,  KC_UP,
         KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD,KC_ROPTN,    MO(3),  KC_LEFT,  KC_DOWN,  KC_RGHT),
     [1] = LAYOUT_ansi_82(
