89c7635d5bb50f2f273ab83259eb77898d64f1a8
Björn Saemundsson <bjornorri@gmail.com>
2025-06-17 22:02:17 +0200

    Proper key down -> key up in tap_hold_modkey

--- users/bjornorri/tap_hold_modkey.c
+++ users/bjornorri/tap_hold_modkey.c
@@ -5,10 +5,11 @@
 typedef struct {
     uint16_t keycode;
     uint16_t tap_key;
-    uint8_t  mod_mask; // supports multiple mods now
+    uint8_t  mod_mask;
     bool     active;
     bool     mod_sent;
     bool     force_tap;
+    bool     tap_key_down;
     uint16_t timer;
 } tap_hold_modkey_key_t;
 
@@ -17,7 +18,7 @@ static uint8_t               tap_hold_modkey_count = 0;
 
 void tap_hold_modkey_register(uint16_t keycode, uint16_t tap_key, uint8_t mod_mask) {
     if (tap_hold_modkey_count < MAX_tap_hold_modkey_KEYS) {
-        tap_hold_modkey_keys[tap_hold_modkey_count++] = (tap_hold_modkey_key_t){.keycode = keycode, .tap_key = tap_key, .mod_mask = mod_mask, .active = false, .mod_sent = false, .force_tap = false, .timer = 0};
+        tap_hold_modkey_keys[tap_hold_modkey_count++] = (tap_hold_modkey_key_t){.keycode = keycode, .tap_key = tap_key, .mod_mask = mod_mask, .active = false, .mod_sent = false, .force_tap = false, .tap_key_down = false, .timer = 0};
     }
 }
 
@@ -25,35 +26,50 @@ bool tap_hold_modkey_process_record(uint16_t keycode, keyrecord_t *record) {
     for (uint8_t i = 0; i < tap_hold_modkey_count; i++) {
         tap_hold_modkey_key_t *k = &tap_hold_modkey_keys[i];
 
-        // Match the registered key
+        // Is this our special key?
         if (keycode == k->keycode) {
             if (record->event.pressed) {
-                k->active    = true;
-                k->mod_sent  = false;
-                k->force_tap = (get_mods() || get_weak_mods());
-                k->timer     = timer_read();
+                k->active       = true;
+                k->mod_sent     = false;
+                k->force_tap    = (get_mods() || get_weak_mods());
+                k->tap_key_down = false;
+                k->timer        = timer_read();
 
                 if (k->force_tap) {
-                    tap_code(k->tap_key);
-                    k->active = false;
+                    // Send tap key down for repeat
+                    register_code(k->tap_key);
+                    k->tap_key_down = true;
                 }
             } else {
                 if (k->mod_sent) {
                     unregister_mods(k->mod_mask);
-                    send_keyboard_report();
+                } else if (k->force_tap && k->tap_key_down) {
+                    unregister_code(k->tap_key);
                 } else if (!k->force_tap) {
-                    tap_code(k->tap_key);
+                    // Standard tap
+                    register_code(k->tap_key);
+                    unregister_code(k->tap_key);
                 }
-                k->active = false;
+
+                send_keyboard_report();
+                k->active       = false;
+                k->tap_key_down = false;
             }
             return false;
         }
 
-        // If this key is active and another key is pressed, register mod immediately
+        // Any other key triggers hold behavior
         if (k->active && record->event.pressed && keycode != k->keycode && !k->mod_sent) {
             register_mods(k->mod_mask);
             send_keyboard_report();
             k->mod_sent = true;
+
+            // If we previously registered tap key, unregister it
+            if (k->tap_key_down) {
+                unregister_code(k->tap_key);
+                send_keyboard_report();
+                k->tap_key_down = false;
+            }
         }
     }
 
